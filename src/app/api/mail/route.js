1c28759b88c99177f05ee53929a0ccc27c40e002
SujanButang <sujanbutang@gmail.com>
2023-10-16 22:57:16 +0545

    fixed email promises

--- src/app/api/mail/route.js
+++ src/app/api/mail/route.js
@@ -9,18 +9,17 @@ export async function GET(req) {
     await connectMongoDB();
     const totalData = await js.countDocuments();
     const users = await user.find({});
-    users.forEach(async (user) => {
+    const emailPromises = users.map(async (user) => {
       const randomIndex = Math.floor(Math.random() * totalData);
       const tip = await js.findOne().skip(randomIndex).exec();
       sendgrid.setApiKey(process.env.SG_API_KEY);
       const msg = {
-        to: user.email, // Change to your recipient
+        to: user.email,
         from: process.env.SENDER,
         subject: "Tip of the day",
         html: `<pre>
         <h2>${tip.title}</h2>
         ${tip.content}
-        
         </pre>
         <br/>
         <br/>
@@ -30,20 +29,17 @@ export async function GET(req) {
         </a>
         </p>`,
       };
-      await sendgrid
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      return sendgrid.send(msg);
     });
-    return new NextResponse(JSON.stringify("Email sent successfully!"), {
+
+    await Promise.all(emailPromises); // Wait for all email promises to resolve
+
+    console.log("All emails sent successfully");
+    return new NextResponse(JSON.stringify("Emails sent successfully!"), {
       status: 200,
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
 }
